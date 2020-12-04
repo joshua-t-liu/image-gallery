42593269753f5a6dec4fd4cc2874bf20b21ca6c3
Joshua Liu <josh.th.liu@gmail.com>
2020-12-03 18:51:10 -0800

    added photos

--- db/helper.js
+++ db/helper.js
@@ -20,27 +20,10 @@ const formatOpenTime = (startDate, endDate) => {
 };
 
 const images = [
-  'IS3jfj0ak22xem0000000000.jpg',
-  'ISjr41rpfv5abo0000000000.jpg',
-  'ISbxffzz9ewajc1000000000.jpg',
-  'ISb957jpzvdv5l1000000000.jpg',
-  'IS7euugyjekp6n0000000000.jpg',
-  'ISjvnu1mztu9e30000000000.jpg',
-  'ISrtvtfm369njd1000000000.jpg',
-  'IS7218mopkhizq0000000000.jpg',
-  'ISqdbzxee17ptk1000000000.jpg',
-  'ISjjqo96yzhnit1000000000.jpg',
-  'ISnmre2qdbxpzs1000000000.jpg',
-  'IS3r51e3jbdaaw0000000000.jpg',
-  'ISjvbla1apr6o50000000000.jpg',
-  'IS76gibg7367ae0000000000.jpg',
-  'ISvkr3zv3tkr421000000000.jpg',
-  'ISfkkph2d4cdoj0000000000.jpg',
-  'IS7eid3rzku28n1000000000.jpg',
-  'ISjfz4xwnvucc60000000000.jpg',
-  'ISbtsugknheaim1000000000.jpg',
-  'ISni0rai7m2sd70000000000.jpg',
-  'ISbxbsamrh0kr21000000000.jpg',
+  './photo1.webp',
+  './photo2.webp',
+  './photo3.webp',
+  './photo4.webp',
 ]
 
 module.exports = { formatOpenTime, images } ;
